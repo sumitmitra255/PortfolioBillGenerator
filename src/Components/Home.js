c9e59a19e5d4a9503bc6e6baed10dde40aee5cdc
sumit mitra <73096616+sumitmitra255@users.noreply.github.com>
2021-02-07 16:14:54 +0530

    'update'

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,52 +11,8 @@ import { useStyles } from '../css/materialuistyles'
 import CanvasJSReact from '..//assets/canvasjs.react'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { Dashboard } from './Dashboard'
-var CanvasJSChart = CanvasJSReact.CanvasJSChart
-const Charts = (props) => {
-	const nocustomers = useSelector((state) => state.customerlist)
-	const options = {
-		backgroundColor: '#4caf50',
-		animationEnabled: true,
-		animationDuration: 3000,
-		width: 320,
-		height: 260,
-		title: {
-			text: 'Total Customers',
-			fontColor: 'white',
-		},
-		subtitles: [
-			{
-				fontColor: 'white',
-				text: `Customers : ${nocustomers.length}`,
-				verticalAlign: 'center',
-				fontSize: 14,
-				dockInsidePlotArea: true,
-			},
-		],
-		data: [
-			{
-				toolTipContent: '{y}',
-				color: 'white',
-				type: 'doughnut',
-				showInLegend: true,
-				indexLabel: '{name}: {y}',
-				indexLabelBackgroundColor: '#4caf50',
-				indexLabelFontColor: 'white',
-				yValueFormatString: '',
-				dataPoints: [{ name: 'No Of Customers', y: nocustomers.length }],
-			},
-		],
-	}
-
-	return (
-		<>
-			<CanvasJSChart
-				options={options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-		</>
-	)
-}
+import { Charts } from './Charts'
+export var CanvasJSChart = CanvasJSReact.CanvasJSChart
 export const Home = (props) => {
 	const dispatch = useDispatch()
 	const token = useSelector((state) => state.logintoken.token)
@@ -71,7 +27,7 @@ export const Home = (props) => {
 		<>
 			<LoggedInNavBar />
 			<Dashboard />
-			<Charts />
+			{/* <Charts /> */}
 		</>
 	)
 }
